7c242f995e39b08d641b8352eb081ea43872ed9e
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-10-14 19:48:09 +0000

    rm1

--- tr/trunk/devices.c
+++
@@ -1,9 +0,0 @@
-#include <io.h>
-
-int sys_write_console(char *buffer,int size)
-{
-  int i;
-  for (i=0; i<size; i++)
-    printc(buffer[i]);
-  return i;
-}
--- tr/trunk/include/utils.h
+++
@@ -1,7 +0,0 @@
-#ifndef UTILS_H
-#define UTILS_H
-
-void copy_data(void *start, void *dest, int size);
-int copy_from_user(void *start, void *dest, int size);
-int copy_to_user(void *start, void *dest, int size);
-#endif
