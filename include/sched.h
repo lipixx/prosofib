181a075425f1f7af9bf9c24b400ee8c0007c5fc8
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-10-15 09:58:46 +0000

    prototip

--- include/sched.h
+++ include/sched.h
@@ -30,6 +30,7 @@ extern union task_union task[NR_TASKS];
 
 /* Inicialitza les dades del proces inicial */
 void init_task0();
+void init_sched();
 struct task_struct* current();
 struct task_struct * list_head_to_task_struct(struct list_head * l);
 
