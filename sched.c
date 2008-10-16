b3a23c74c81b9189bd8cedecc8043816d8fcc008
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-10-16 11:50:19 +0000

    sched.c

--- sched.c
+++ sched.c
@@ -17,10 +17,15 @@ void init_sched()
 
 void init_task0(int first_ph)
 {
+  int i = 0;
   task[0].task.pid = pid++;
   task[0].task.quantum = 0;
   task[0].task.tics_cpu = 0;
-  task[0].task.pagines_fisiques[0] = first_ph; 
+  task[0].task.pagines_fisiques[0] = first_ph;
+  
+  for (i=0;i<NUM_PAG_CODE+NUM_PAG_DATA;i++)
+    task[0].task.pagines_fisiques[i] = -1;
+  
   list_add(&(task[0].task.run_list),&runqueue);
 }
 
