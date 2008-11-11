865e1175b7a60206362e7cc669f4350810cd68b0
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-11 19:03:55 +0000

    EstructuresOK

--- trunk/sys.c
+++ trunk/sys.c
@@ -42,9 +42,8 @@ sys_write (int fd, char *buffer, int size)
       copy_from_user (buffer, buff_aux, writeSize);
       struct task_struct* actual = current();
       struct fitxers_oberts* tfo = actual->taula_canals[fd];
-      int f = tfo->refs;
-      //struct file* f= tfo->opened_file;
-      //struct file_operations* op= f->operations;
+      struct file* f= tfo->opened_file;
+      struct file_operations* op= f->operations;
       //ncPrinted +=op->sys_write_dev(buff_aux,writeSize);
       //ncPrinted += (current()->taula_canals[fd])->file->operations.sys_write_dev(buff_aux,writeSize);  
       //ncPrinted += sys_write_console (buff_aux, writeSize);
