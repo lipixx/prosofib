11473756e069ad5f5559a752f37f4e8118c8f78c
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-11 15:55:32 +0000

    EstructuresOK

--- include/sf.h
+++ include/sf.h
@@ -1,6 +1,7 @@
 /*
  * sf.h - Sistem de Fitxers: 
  */
+#include <types.h>
 
 #ifndef __SF_H__
 #define __SF_H__
@@ -14,38 +15,39 @@
 #define MAX_FILES 10
 #define MAX_NAME_LENGTH 10
 
+void init_fat();
+
 /* Sistema de fitxers */
 Byte fat[MAX_BLOCKS];
 int bloc_de_la_llibertat;
 
 /* Disc */
 Byte disk[MAX_BLOCKS][BLOCK_SIZE];
-int directori;//[MAX_FILES]; 
-void init_fat();
+int directori[MAX_FILES]; 
 
 struct file_operations{
-//int (*sys_open_dev)(const char *, int);
-//int (*sys_close_dev)(int);
-//int (*sys_read_dev)(int, char*, int);
-//int (*sys_write_dev)(int, char*, int);
+int (*sys_open_dev)(const char *, int);
+int (*sys_close_dev)(int);
+int (*sys_read_dev)(int, char*, int);
+int (*sys_write_dev)(int, char*, int);
 int a;
-}
+};
 
 struct file{
 char nom[MAX_NAME_LENGTH];
 int mode_acces_valid;
-//struct file_operations *operations;
+struct file_operations *operations;
 int first_block;
 int size;
-// int n_blocs;
-}
-/*
+int n_blocs;
+};
+
 struct taula_fitxers_oberts {
 int refs;
 int mode_acces;
 int lseek;
-//struct file* file;
-}
-*/
+struct file* file;
+};
+
 
 #endif /* __SF_H__ */
