028e85361f45ce54f6a46db4b495689f3824ee22
one.neuron <one.neuron@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-20 20:59:13 +0000

    fork

--- trunk/include/devices.h
+++ trunk/include/devices.h
@@ -8,6 +8,7 @@ int sys_close_file(int fd);
 int sys_read_file(int fd, char* buffer, int size);
 int sys_write_file(int fd, char* buffer, int size);
 int sys_unlink_file(const char *path);
-
+int sys_close_console(int fd);
+int sys_close_keyboard(int fd);
 
 #endif /* DEVICES_H__ */
