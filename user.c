804de14f9a96dc14096117a6be72eacda6ebac75
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-21 16:37:28 +0000

    va

--- user.c
+++ user.c
@@ -7,20 +7,14 @@ void runjp ();
 
 int __attribute__ ((__section__ (".text.main"))) main (void)
 {
-  printf ("\nBenvinguts a ZeOS!");  
-  
-  int dupx;
-  int forky;
-  int fd;
-  
-  close(0);
-  // close(1);
-  //  close(2);
-  
-  open("console",O_WRONLY);
-  perror();
-  
-  
+  // printf ("\nBenvinguts a ZeOS!");  
+ 
+  /* __asm__ __volatile__ ("movl $40,%%eax\n"
+			"int $0x80\n"
+			:
+			:
+			);
+  */
   //provar_fork (9); //No sobrepassar NR_TASKS
 
   //provar_get_stats ();
@@ -35,7 +29,7 @@ int __attribute__ ((__section__ (".text.main"))) main (void)
 
   /* JOCS DE PROVES DE LA PRIMERA ENTREGA */
 
-  //runjp (); //Hi ha que posar NUM_PAG_DATA = 8
+  runjp (); //Hi ha que posar NUM_PAG_DATA = 8
 
   //run2_jp();
 
