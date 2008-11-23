347b7cc0fc12e0d6e2b32ed732dee5714de77a74
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-23 21:04:28 +0000

    commiting

--- include/utils.h
+++ include/utils.h
@@ -5,6 +5,7 @@ void copy_data (void *start, void *dest, int size);
 int copy_from_user (void *start, void *dest, int size);
 int copy_to_user (void *start, void *dest, int size);
 int strcmp(const char * s1, const char * s2);
-//void itoa (int n, char *buffer);
+int strlen(const char * s1);
+void itoa (int n, char *buffer);
 
 #endif
