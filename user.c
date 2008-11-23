2e1fca9701895747c81cb47cdadd79d3024d0d7a
lipixx <lipixx@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-23 04:46:39 +0000

    commiting

--- user.c
+++ user.c
@@ -39,20 +39,28 @@ int __attribute__ ((__section__ (".text.main"))) main (void)
   // int fd = open("patata",O_WRONLY|O_CREAT);
 
   //run2_jp();
- struct dir_ent buffer;
+  struct dir_ent buffer;
+  //char buffer3[512];
+  char buffer2[512];
   int fd;
   
-  open ("patata",O_RDWR|O_CREAT);
-  open ("nonara",O_RDWR);
+  fd = open ("patata",O_RDWR|O_CREAT);
 
-  for (fd = 0; fd < MAX_FILES; fd++)
-    {
-      readdir(&buffer,fd);
-      printf("\nnom: ");
-      printf(buffer.nom);
-      printf("\nsize: ");
-      printint(buffer.size);
-      }
+  write(fd,"1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123FXP789012345678901234567890",280);
+  write(fd,"HOLA",4);
+
+  close(fd);
+  fd = open("patata",O_RDWR);
+  
+  readdir(&buffer,2);
+  printf("\nnom: ");
+  printf(buffer.nom);
+  printf("\nsize: ");
+  printint(buffer.size);
+
+  read(fd,buffer2,284);
+  write(1,buffer2,284);
+
   while (1);
   return 0;
 }
