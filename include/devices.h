d19486d45d7e8d48621e697a972a3364651a0bfe
one.neuron <one.neuron@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-23 18:43:39 +0000

    fork

--- include/devices.h
+++ include/devices.h
@@ -13,6 +13,7 @@ int sys_write_file(int fd, char* buffer, int size);
 int sys_unlink_file(const char *path);
 int sys_close_console(int fd);
 int sys_close_keyboard(int fd);
+void actualitzar_pendents(int size);
 void avanzar();
 void bloquejar_teclat(struct task_struct *task);
 void desbloquejar_teclat(struct task_struct *task );
