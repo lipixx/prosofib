900df9649dc5e9ccdcef5c91e3dde33522660f40
one.neuron <one.neuron@2206873e-99fd-11dd-a26a-893737b22a74>
2008-11-23 00:45:57 +0000

    fork

--- include/devices.h
+++ include/devices.h
@@ -1,6 +1,9 @@
 #ifndef DEVICES_H__
 #define  DEVICES_H__
 
+#include <sched.h>
+
+void init_devices();
 int sys_write_console (int fd, char *buffer, int size);
 int sys_read_keyboard (int fd, char *buffer, int size);
 int sys_open_file(const char *path, int mode_acces);
@@ -10,5 +13,9 @@ int sys_write_file(int fd, char* buffer, int size);
 int sys_unlink_file(const char *path);
 int sys_close_console(int fd);
 int sys_close_keyboard(int fd);
+void avanzar();
+void bloquejar_teclat(struct task_struct *task);
+void desbloquejar_teclat(struct task_struct *task );
+void anar_al_circ(struct task_struct *bloq, int size);
 
 #endif /* DEVICES_H__ */
--- include/keyboard.h
+++ include/keyboard.h
@@ -2,4 +2,29 @@
 #define __KEYBOARD_H__
 
 
+#define CIRCULAR_SIZE 256
+
+#include <list.h>
+
+/*
+struct keyq {
+  struct list_head *queue;   
+  int chars_pendents;
+  int buffer;
+  int size;
+} keyboard_queue;  
+*/
+struct list_head keyboard_queue;
+
+ 
+/* keyboard_queue es la cua on tindrem bloquejats els procesos pendents de llegir algun caracter per teclat */
+/* Fins que un proces no hagi acabat de fer la lectura de tots els caracters no podra sortir de la cua keyboard_queue */
+
+char buffer_circular[CIRCULAR_SIZE];
+int pos;
+int inici;
+int circ_chars; /* Indica el nombre de caracters que hi ha dins el buffer circular */
+
+
+
 #endif /* __KEYBOARD_H__ */
